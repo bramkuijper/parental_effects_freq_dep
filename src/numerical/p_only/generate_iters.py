b5d2e6c9a6d82a3d8e04d7ece6a3ce4148f142dd
Bram Kuijper <bramkuijper@gmail.com>
2016-12-11 09:14:30 +0000

    now running single p numerical iterations

--- src/numerical/p_only/generate_iters.py
+++ src/numerical/p_only/generate_iters.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-step = 0.02
+step = 0.05
 vvals = list(np.arange(0.01, 1.0, step))
 cvals = list(np.arange(0.01, 1.0, step))
 
@@ -15,22 +15,26 @@ phh_init = [ 0.5 ]
 
 ctr = 0
 
+error = [ 0, 0.02 ] 
+
 for v_i in vvals:
     for c_i in cvals:
-        for d_i in d:
-            for pdh_i in pdh_init: 
-                for phh_i in phh_init:
-                    print("echo " + str(ctr))
-                    ctr+=1
-                    print(exe + " 1000000 " 
-                            + str(d_i) + " " 
-                            + str(v_i) + " " 
-                            + str(c_i) + " "
-                            + str(0.33) + " "
-                            + str(0.33) + " "
-                            + str(pdh_i) + " "
-                            + str(phh_i) + " "
-                            + " 0.1 0.1 0.1 "
-                            + " 1.0 1.0 1.0 1.0 "
-                            )
+        for error_i in error:
+            for d_i in d:
+                print("echo " + str(ctr))
+                ctr+=1
+                print(exe + " 1000000 " 
+                        + str(d_i) + " " 
+                        + str(1.0 - v_i/2.0) + " "  # md_0
+                        + str(1.0 - v_i) + " " # mh_1
+                        + str(1.0) + " " # md_1
+                        + str(1.0 - (v_i - c_i)/2.0) + " " # mh_2
+                        + str(0.5) + " " # p_init
+                        + str(error_i) + " " # err
+                        + str(0.33) + " "
+                        + str(0.33) + " "
+                        + str(0.5) + " "
+                        + " 0.1 0.1 0.1 "
+                        + " 1.0 1.0 1.0 1.0 "
+                        )
 
--- src/numerical/p_only/plot_sims.py
+++ src/numerical/p_only/plot_sims.py
@@ -20,7 +20,18 @@ f = open(filename)
 fl = f.readlines()
 f.close()
 
-nrow = len(fl)-8
+# find the parameters
+parline = -1
+
+for idx, line in enumerate(fl):
+    if re.match("^d.*",line) != None:
+        parline = idx - 1;
+        break;
+
+if parline == -1:
+    nrow = len(fl)
+else:
+    nrow = parline -2  
 
 
 data = pd.read_csv(filename, sep=";", nrows=nrow)
